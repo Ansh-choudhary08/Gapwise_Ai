685ab9c22a733cced75ca0a9fedca12085f6080e
[Ansh-choudhary08] <[anshchoudhary176@gmail.com]>
2026-09-09 10:52:32 +0530

    update backend and frontend url

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://gapwise-ai-yylj.onrender.com",
     withCredentials: true,
 })
 
